c6ccd00a70ebfb2482e6a1cf9cc3266a135ccf77
GanaF4ll <ganafall9498@gmail.com>
2024-04-20 13:26:45 +0200

    Update Navbar component to include new button and navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,41 @@
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-orange-primary flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/LogoLetters.png" alt="Logo" width={74} height={29} />
-        <ul className="hidden h-full gap-12 lg:flex">
-          {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key}>
-              {link.label}
-            </Link>
-          ))}
-        </ul>
       </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Se connecter"
+          icon="/user.svg"
+          variant="btn_blue_primary"
+        />
+      </div>
+      <Image
+        src="/menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
