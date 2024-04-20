448d7bb3363f143ddde4012051953c8d9aae28ed
GanaF4ll <ganafall9498@gmail.com>
2024-04-20 12:56:49 +0200

    Add navigation links to Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,6 +7,13 @@ const Navbar = () => {
     <nav className="border-2 border-orange-primary flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/LogoLetters.png" alt="Logo" width={74} height={29} />
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.label}
+            </Link>
+          ))}
+        </ul>
       </Link>
     </nav>
   );
