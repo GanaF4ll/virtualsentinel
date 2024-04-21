522a2e540eb9cc4953edef22a76ea0fc257399e8
GanaF4ll <ganafall9498@gmail.com>
2024-04-21 20:38:47 +0200

    Add footer component with links and social icons

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,16 @@ export const NAV_LINKS = [
   { href: "/", key: "tarifs ", label: "Tarifs " },
   { href: "/", key: "contact", label: "Contact" },
 ];
+
+// FOOTER
+export const FOOTER_LINKS = [
+  {
+    title: "Plus d'infos",
+    links: ["A propos de nous", "Nous contacter", "FAQ"],
+  },
+];
+
+export const SOCIALS = {
+  title: "Réseaux sociaux",
+  links: ["/facebook.svg", "/instagram.svg", "/linkedin.svg"],
+};
