df4b9daf6e332c19e4d8a26f7ef53f07a556cf67
GanaF4ll <ganafall9498@gmail.com>
2024-07-07 08:59:54 +0200

    :lipstick: UI UserReview

--- components/Download.tsx
+++ components/Download.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import Button from "./Button";
 import Image from "next/image";
 import { museoModerno, montSerrat } from "../app/fonts";
+import UserReview from "./UserReview";
 
 const Download = () => {
+  const userReview = {
+    name: "John Doe",
+    photo: "/background.jpg",
+    rating: 4,
+    comment: "This is an amazing product! Highly recommend it.",
+  };
   return (
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
@@ -36,6 +43,7 @@ const Download = () => {
         <div className="flex flex-1 items-center justify-end">
           <Image src="/phone.png" alt="phones" width={550} height={870} />
         </div>
+        <UserReview user={userReview} />
       </div>
     </section>
   );
